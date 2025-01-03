6cc916a0273346c1863e10312d8e4d04fb563ec6
AMP95 <aikosan95@gmail.com>
2025-01-03 22:45:21 +0700

    add configs in Program

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,6 +1,10 @@
-
 using DAL;
+using DTOs;
+using Exam.BackgroundServices;
+using Exam.ResultServices;
+using MediatRepos;
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace Exam
 {
@@ -10,11 +14,18 @@ namespace Exam
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Logging.AddProvider(new Log4NetProvider("log4net.config"));
+
             builder.Services.AddControllers().AddNewtonsoftJson();
 
-            builder.Services.AddOpenApi();
-            builder.Logging.AddProvider(new Log4NetProvider("log4net.config"));
-            builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+            builder.Services.AddTransient<IRepository, ContextRepository>();
+
+            builder.Services.AddSingleton<ResultService>();
+            builder.Services.AddSingleton<RequestStatusService>();
+            builder.Services.AddSingleton<GetService>();
+            builder.Services.AddSingleton<UpdateService>();
 
             builder.Services.AddCors(options =>
             {
@@ -24,11 +35,23 @@ namespace Exam
                 });
             });
 
+
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHostedService(sp => sp.GetService<GetService>());
+            builder.Services.AddHostedService(sp => sp.GetService<UpdateService>());
+
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(Add<TruckDto>).Assembly);
+            });
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
